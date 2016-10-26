09db1490739a627e9c68e04094531d0feff61b87
Kyle Murdoch <kyle.murdoch576@gmail.com>
2016-10-26 11:10:19 -0500

    Revised some of the input validation and added some comments.

--- CardGames.java
+++ CardGames.java
@@ -44,24 +44,29 @@ public class CardGames { // created 10/07/16 @ 11:56pm by kmurdoch
             if (action.compareTo("ADD CREDITS") == 0) {
                 System.out.println("How many credits would you like to add?");
                 System.out.print("(Credits: " + player.getCredits() + ") ==> ");
-                int numCredits = input.nextInt();
-                if (numCredits > 0) { // validate input
-                    player.addCredits(numCredits);
+                if (input.hasNextInt()) { // validate input
+                    int numCredits = input.nextInt();
+                    if (numCredits > 0) {
+                        player.addCredits(numCredits); // set credits
+                    }
+                    else {
+                        System.out.println("Not a valid number of credits.");
+                    }
                 }
                 else {
                     System.out.println("INVALID INPUT");
                 }
             }
-            else if (action.compareTo("MAX BET") == 0) {
+            else if (action.compareTo("MAX BET") == 0) { // start game of poker with maximum bet of 5
                 if (player.getCredits() >= 5) {
                     player.setBet(5);
                     playing = true;
                 }
-                else if (player.getCredits() > 0) {
+                else if (player.getCredits() > 0) { // if player doesn't have at least 5 credits, set bet to number of credits
                     player.setBet(player.getCredits());
                     playing = true;
                 }
-                else {
+                else { // print error if user has no credits
                     System.out.println("Insufficient credits");
                 }
             }
@@ -69,19 +74,24 @@ public class CardGames { // created 10/07/16 @ 11:56pm by kmurdoch
                 boolean validBet = false;
                 while (!validBet) { // validate input
                     System.out.print("Enter bet (MAX 5) ==> ");
-                    int bet = input.nextInt();
-                    if (bet > 0 && bet <= 5 && player.getCredits() >= bet) {
-                        player.setBet(bet);
-                        validBet = true;
-                        playing = true;
-                    }
-                    else if (player.getCredits() < bet) {
-                        System.out.println("Insufficient credits");
-                        break;
+                    if (input.hasNextInt()) {
+                        int bet = input.nextInt();
+                        if (bet > 0 && bet <= 5 && player.getCredits() >= bet) {
+                            player.setBet(bet);
+                            validBet = true;
+                            playing = true;
+                        }
+                        else if (player.getCredits() < bet) {
+                            System.out.println("Insufficient credits");
+                            break;
+                        }
+                        else {
+                            System.out.println("Invalid bet");
+                            break;
+                        }
                     }
                     else {
-                        System.out.println("Invalid bet");
-                        break;
+                        System.out.println("INVALID INPUT");
                     }
                 }
             }
@@ -107,23 +117,48 @@ public class CardGames { // created 10/07/16 @ 11:56pm by kmurdoch
                 else {
                     System.out.println("High Card");
                 }
-                int numHold;
-                do { // validate input
-                    numHold = input.nextInt();
-                    if (numHold < 0 || numHold > 5){
-                        System.out.println("Please enter a valid number");
+                int numHold = 0;
+                System.out.print("==> ");
+                
+                do {
+                    if (input.hasNextInt()) { // ensure that the next input is an integer
+                        do { // validate input
+                            numHold = input.nextInt();
+                            if (numHold < 0 || numHold > 5){
+                                System.out.println("Please enter a value from 0 to 5");
+                                System.out.print("==> ");
+                            }
+                        } while (numHold < 0 || numHold > 5);
                     }
-                } while (numHold < 0 || numHold > 5);
+                    else {
+                        System.out.println("INVALID INPUT");
+                    }
+                } while (!input.hasNextInt());
                 
                 if (numHold > 0 && numHold <= 5) {
-                    System.out.println("Enter which cards to hold");
+                    System.out.println("Enter the cards you want to hold");
                     System.out.println("ex. '1 3 4' will hold cards in position 1, 3, and 4");
+                    System.out.print("==> ");
                 }
-                int[] hold = new int[6];
+                int[] hold = new int[6]; // create an array with max value possible
                 for (int i = 0; i < numHold; i++) {
-                    hold[input.nextInt()]++;
+                    do {
+                        if (input.hasNextInt()) { // validate each card entered
+                            int num = input.nextInt();
+                            if (num > 0 && num <= 5) {
+                                hold[num]++;
+                            }
+                            else {
+                                System.out.println("Enter a number between 1 and 5");
+                                i--;
+                            }
+                        }
+                        else {
+                            System.out.println("INVALID INPUT");
+                        }
+                    } while (!input.hasNextInt());
                 }
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < 5; i++) { // replace cards being traded in hand
                     if (hold[i + 1] == 0) {
                         hand.setNewCard(i, deck);
                     }
@@ -146,8 +181,8 @@ public class CardGames { // created 10/07/16 @ 11:56pm by kmurdoch
         return 0;
     } 
     
-    public static void checkRandomizer() {
-        int[] array = new int[11];
+    public static void checkRandomizer() { // runs 1 Million hands of poker and prints the total values and percentages
+        int[] array = new int[11];         // used to test the odds for the randomizer
         for (int i = 0; i < 1000000; i++) {
             Deck deck = new Deck();
             deck.shuffleDeck();
@@ -163,7 +198,7 @@ public class CardGames { // created 10/07/16 @ 11:56pm by kmurdoch
         }
     }
     
-    public static void singleHandTest() {
+    public static void singleHandTest() { // prints a single randomized hand along with it's value
         Deck deck = new Deck();
         deck.shuffleDeck();
         FiveCardPokerHand hand = new FiveCardPokerHand(deck);
